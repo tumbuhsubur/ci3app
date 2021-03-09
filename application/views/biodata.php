28ddfd727100071e8e3794c1186823fbd776ec8c
tumbuhsubur <61866257+tumbuhsubur@users.noreply.github.com>
2021-03-09 21:59:51 +0700

    Initial commit

---
+++ application/views/biodata.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+  <title>Biodata Author</title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/bootstrap.css" />
+
+<body>
+  <div class="col-md-auto text-center mt-5 mb-5">
+    <h1>Biodata</h1>
+  </div>
+  <div class="container">
+    <div class="row align-items-start">
+      <div class="col-md-auto">
+        <img src="<?php echo base_url(); ?>/public/images/wings.png" class="img-thumbnail" alt="Eka Januard F.">
+      </div>
+      <div class="col-md-auto text-center mt-5">
+        <h1>Eka Januard Fidaus</h1>
+        <h1>19510006</h1>
+        <h2>Front End Newbie</h2>
+        <br>
+        <h3>Universitas Gajayana Malang</h3>
+        <h4>Sistem Informasi 2019</h4>
+      </div>
+    </div>
+  </div>
+</body>
+</head>
+
+</html>
